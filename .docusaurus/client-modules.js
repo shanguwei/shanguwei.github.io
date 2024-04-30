c59274c8324ad0b588eeb04dba2c72b280dc7dce
Shangu <shangu127.top@gmail>
2024-04-30 13:41:00 +0800

    Update .gitignore

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/shangu/code/ctfchat/CTFChat/my-website/node_modules/infima/dist/css/default/default.css"),
-  require("/home/shangu/code/ctfchat/CTFChat/my-website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/shangu/code/ctfchat/CTFChat/my-website/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/shangu/code/ctfchat/CTFChat/my-website/src/css/custom.css"),
-];
